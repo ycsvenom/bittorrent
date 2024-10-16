46a5d6d828c86d63923085874ef6f3f8193457f7
ycsvenom <ycsvenom@gmail.com>
2024-10-17 01:30:19 +0300

    codecrafters submit [skip ci]

--- tests/bencode_tests.cpp
+++ tests/bencode_tests.cpp
@@ -1,10 +1,18 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "src/bencode.hpp"
+#include "../src/bencode.hpp"
 
-TEST_CASE("testing decode_bencoded_value")
+TEST_CASE("decoding strings decode_bencoded_value")
 {
+	CHECK(decode_bencoded_value("0:").dump() == "\"\"");
 	CHECK(decode_bencoded_value("5:hello").dump() == "\"hello\"");
+	CHECK(decode_bencoded_value("11:hello world").dump() == "\"hello world\"");
 	CHECK_THROWS_AS(decode_bencoded_value("a:hello").dump(), std::runtime_error);
 	CHECK_THROWS_AS(decode_bencoded_value("5hello").dump(), std::runtime_error);
+}
+
+TEST_CASE("decoding integers decode_bencoded_value")
+{
+	CHECK(decode_bencoded_value("i52e") == "52");
+	CHECK(decode_bencoded_value("i0e") == "0");
+	CHECK(decode_bencoded_value("i-52e") == "-52");
 }
